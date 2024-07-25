7914b77e22d41d882ef6a9b6d7ce0e566ae5fb48
Gabriel Drumond <gabriel.drumond@ezpag.com>
2024-07-25 14:22:35 -0300

    feat/convert_jpeg

---
+++ convert_JPEG.py
@@ -0,0 +1,13 @@
+def convert_to_jpeg(file_path) -> str:
+
+        image = Image.open(file_path)
+        
+        if image.mode in ("RGBA", "P"):
+            image = image.convert("RGB") #converto para RGB por que png tem transparencia e jpeg não
+            
+        base = os.path.splitext(file_path)[0] #removendo a extensão
+        new_image_path = f"{base}.jpeg" #incluindo a extensão jpeg
+        
+        image.save(new_image_path, "JPEG")
+        
+        return new_image_path
